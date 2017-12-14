485bf2c189888d86fd54f1434d9799ea15d1ec9b
chushaa <31249563+chushaa@users.noreply.github.com>
2017-12-14 12:16:36 -0600

    Adding of Data Folder and the JSON + JS File

---
+++ data/data.js
@@ -0,0 +1,74 @@
+var iceCream = [
+    {
+    "name": "Cookie Monster",
+    "pricePerScoop": 3.25,
+    "isPeanut": "no",
+    "isGluten": "no"
+    },
+    {
+    "name": "Butter My Pecan",
+    "pricePerScoop": 3.25,
+    "isPeanut": "no",
+    "isGluten": "yes"
+    },
+    {
+    "name": "The Purist",
+    "pricePerScoop": 2.75,
+    "isPeanut": "no",
+    "isGluten": "yes"
+    },
+    {
+    "name": "Time for Reeses",
+    "pricePerScoop": 3,
+    "isPeanut": "yes",
+    "isGluten": "no"
+    },
+    {
+    "name": "Valentine",
+    "pricePerScoop": 3,
+    "isPeanut": "no",
+    "isGluten": "yes"
+    },
+    {
+    "name": "Minty Satisfaction",
+    "pricePerScoop": 3,
+    "isPeanut": "no",
+    "isGluten": "no"
+    },
+    {
+    "name": "Mr. Sherbet",
+    "pricePerScoop": 3,
+    "isPeanut": "no",
+    "isGluten": "no"
+    },
+    {
+    "name": "Club Herbal",
+    "pricePerScoop": 2.75,
+    "isPeanut": "no",
+    "isGluten": "no"
+    },
+    {
+    "name": "Rocky Balboa",
+    "pricePerScoop": 3.25,
+    "isPeanut": "yes",
+    "isGluten": "yes"
+    },
+    {
+    "name": "Barney",
+    "pricePerScoop": 2.75,
+    "isPeanut": "no",
+    "isGluten": "no"
+    },
+    {
+    "name": "3AM Madness",
+    "pricePerScoop": 3.25,
+    "isPeanut": "yes",
+    "isGluten": "no"
+    },
+    {
+    "name": "Organ Donors",
+    "pricePerScoop": 3,
+    "isPeanut": "no",
+    "isGluten": "no"
+    }
+];
