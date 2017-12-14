e213e072413def55e6e95b6e5c14dbdc419fa22a
chushaa <31249563+chushaa@users.noreply.github.com>
2017-12-14 12:52:02 -0600

    Updated Data.js

--- data/data.js
+++ data/data.js
@@ -3,72 +3,84 @@ var iceCream = [
     "name": "Cookie Monster",
     "pricePerScoop": 3.25,
     "isPeanut": "no",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/cookie-monster.jpg"
     },
     {
     "name": "Butter My Pecan",
     "pricePerScoop": 3.25,
     "isPeanut": "no",
-    "isGluten": "yes"
+    "isGluten": "yes",
+    "image": "images/butter-pecan.jpg"
     },
     {
     "name": "The Purist",
     "pricePerScoop": 2.75,
     "isPeanut": "no",
-    "isGluten": "yes"
+    "isGluten": "yes",
+    "image": "images/the-purist.jpg"
     },
     {
     "name": "Time for Reeses",
     "pricePerScoop": 3,
     "isPeanut": "yes",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/time-for-reeses.jpg"
     },
     {
     "name": "Valentine",
     "pricePerScoop": 3,
     "isPeanut": "no",
-    "isGluten": "yes"
+    "isGluten": "yes",
+    "image": "images/strawberry.jpg"
     },
     {
     "name": "Minty Satisfaction",
     "pricePerScoop": 3,
     "isPeanut": "no",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/mint-cookie.jpg"
     },
     {
     "name": "Mr. Sherbet",
     "pricePerScoop": 3,
     "isPeanut": "no",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/mr-sherbet.jpg"
     },
     {
     "name": "Club Herbal",
     "pricePerScoop": 2.75,
     "isPeanut": "no",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/club-herbal"
     },
     {
     "name": "Rocky Balboa",
     "pricePerScoop": 3.25,
     "isPeanut": "yes",
-    "isGluten": "yes"
+    "isGluten": "yes",
+    "image": "images/rocky-balboa.jpg"
     },
     {
     "name": "Barney",
     "pricePerScoop": 2.75,
     "isPeanut": "no",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/barney.jpg"
     },
     {
     "name": "3AM Madness",
     "pricePerScoop": 3.25,
     "isPeanut": "yes",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/3am-madness.jpg"
     },
     {
     "name": "Organ Donors",
     "pricePerScoop": 3,
     "isPeanut": "no",
-    "isGluten": "no"
+    "isGluten": "no",
+    "image": "images/organ-doners.jpg"
     }
 ];
